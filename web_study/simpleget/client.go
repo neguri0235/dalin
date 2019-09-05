68fe3d6189ec526160cf9f44fa462fd3f56f8ab8
neguri <projekct@gmail.com>
2019-09-06 00:22:02 +0900

    add server

---
+++ web_study/simpleget/client.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://localhost:18888")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Headers:", resp.Header)
+	log.Println(string(body))
+	log.Println("status:", resp.Status)
+	log.Println("statusCode:", resp.StatusCode)
+	log.Println("Content-Length:", resp.Header.Get("Content-Length"))
+}
